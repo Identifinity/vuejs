4be193165068974bcf0995f7c6f7a3af464a5128
Daniel <ihatephp@protonmail.com>
2019-01-17 00:20:29 +0100

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Site msg="Witaj!" today="16/01/2019" weather="sunny"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Site from "./components/Site.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Site
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Site.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="container">
+    <div>
+      Wiadomości dnia: {{msg}}
+      <br>
+      Dziś jest {{today}}
+      <br>
+      Pogoda: {{weather}}
+    </div>
+
+    <div id="counter" v-bind:class="{ gtr: isGreater, lr: !isGreater}">{{count}}</div>
+    <br>
+    <button id="btn-counter" v-on:click="countUp">Count Up!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "site",
+  props: ["msg", "today", "weather"],
+  data() {
+    return {
+      count: 0,
+      isGreater: false
+    };
+  },
+  methods: {
+    countUp() {
+      this.count++;
+      this.isGreater = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+#container {
+  font-size: 24px;
+  color: black;
+}
+
+.gtr {
+  background: #3ccf02;
+  color: white;
+}
+
+.lr {
+  background: rgba(30, 30, 30, 0.5);
+}
+
+#counter {
+  font-size: 60px;
+  border-radius: 20px;
+  display: inline-block;
+  line-height: 50px;
+  padding: 20px;
+  margin: 20px auto;
+}
+
+#btn-counter {
+  width: 120px;
+  height: 50px;
+  border: none;
+  cursor: pointer;
+  font-size: 20px;
+  border-radius: 20px;
+  font-weight: bold;
+
+  transition: all 0.3s;
+  outline: 0;
+}
+
+#btn-counter:hover {
+  background: gold;
+}
+</style>
+
+
+
